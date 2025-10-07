642af1de4c491ca4c8de3e352e870404aae64f38
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2025-10-07 10:55:50 -0400

    Entry widget and color change

--- gui.py
+++ gui.py
@@ -1,8 +1,17 @@
 from tkinter import * 
 
+def submit():
+    return
+
 window = Tk()
 window.title("PLACEHOLDER TITLE")
 window.geometry("400x300")
 window.config(background="#550000")
+
+submit = Button(window, text="Enter", command=submit)
+
+entry = Entry()
+entry.pack()
+
 
 window.mainloop()
