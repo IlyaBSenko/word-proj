18e0c37a4cdf77b2e5f075e8c1afba69b2d9054d
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2025-10-09 10:40:46 -0400

    Put all prev code in gui and added some spice

--- gui.py
+++ gui.py
@@ -3,7 +3,11 @@ import random
 
 
 def submit():
-    return
+    guess = entry.get().strip()
+    if guess.lower() == random_word:
+        result_label.config(text="Lol nice")
+    else:
+        result_label.config(text="Wrong, die")
 
 window = Tk()
 window.title("PLACEHOLDER TITLE")
@@ -13,23 +17,25 @@ window.config(background="#550000")
 with open("words.txt", "r") as f:
     exec(f.read()) # executive mode cmon now (Top Dogs Only)
     
+random_word = random.choice(easy_words) # select a random word from the list
+sorted_word = sorted(random_word)
+sorted_string = "".join(sorted_word)
 
-random_word_text = random.choice(easy_words) # wtf is choice, (select a random element from a sequence, aka easy_words in words)
 
-# title label
+# GUI ELEMENTS
 title_label = Label(window, text="Word Guessr", font=("Segoe Script Bold", 24, "bold"), bg="#550000")
 title_label.pack(pady=20)
 
 Guess_word_label = Label(window, text="Guess the word:", font=("Palatino Linotype Bold Italic", 16), bg="#550000")
 Guess_word_label.pack(pady=21)
 
-rand_word = Label(window, text=random_word_text, font=("Palatino Linotype Bold Italic", 12), bg="#550000")
+rand_word = Label(window, text=sorted_string, font=("Palatino Linotype Bold Italic", 12), bg="#550000")
 rand_word.pack()
-
-
 
 entry = Entry(window, font=("Palatino Linotype Bold Italic", 14))
 entry.place(relx=0.5, rely=0.7, anchor="center") 
+
+result_label = Label(window, text="", bg="#550000", fg="white", font=("Palatino Linotype Bold Italic", 14))
 
 
 submit_button = Button(window, text="Enter", command=submit)
